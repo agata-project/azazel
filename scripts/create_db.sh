199cc7e9686b3c909d0b0056da37cc160b43d9e3
Marcos Felipe Eipper <marcos@eipper.com.br>
2018-07-09 23:48:02 -0300

    Fix create_db script in order to support Macs. 💻🤓 (#1)
    🕶

--- scripts/create_db.sh
+++ scripts/create_db.sh
@@ -7,4 +7,9 @@ commands=(
   "ALTER USER $2 CREATEDB;"
 )
 
+if [ $(uname) == "Darwin" ]
+then
+for command in "${commands[@]}"; do psql postgres -c "$command"; done
+else
 for command in "${commands[@]}"; do sudo -u postgres psql -c "$command"; done
+fi
